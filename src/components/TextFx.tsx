33f7abf9fd38600efad2e83c4489ab06b0da2098
pce <info@pc-e.org>
2022-11-23 01:44:44 +0100

    feat: TextFx

--- src/components/TextFx.tsx
+++ src/components/TextFx.tsx
@@ -1,27 +1,37 @@
 
 
 /** @jsxImportSource @emotion/react */
-
 // import { css, keyframes } from "@emotion/react";
-import { ReactElement, ReactNode } from "react";
-import { animated, useSpring } from "@react-spring/web";
+import { animated, useTrail } from "@react-spring/web";
+import React from "react";
 
 interface ITextFx {
-    children: string[] | ReactElement | ReactElement[] | ReactNode;
+    children: React.ReactElement | React.ReactElement[] | React.ReactNode;
     // mode: TextFxMode | undefined;
-}
+};
 
 
 export const TextFx = ({ children, ...props }: ITextFx) => {
-    const springStyle = useSpring(
-            { opacity: 1, from: { opacity: 0 }, delay: 350 }
-        );
 
-    let text = String(children);
+    const [open, setOpen] = React.useState<boolean>(false);
 
-    return <>{text.split("").map((item: string, index: number) => {
-        return <animated.span key={index} style={springStyle} {...props}>
-            {item}
+    React.useEffect(() => {
+        setTimeout(() => { setOpen(true); }, 800)
+    }, []);
+
+    // React.Children.toArray ?
+    let textItems = String(children).split("");
+
+    const trail = useTrail(textItems.length, {
+        config: { mass: 5, tension: 2000, friction: 200 },
+        opacity: open ? 1 : 0,
+        x: open ? 0 : 20,
+        height: open ? 110 : 0,
+        from: { opacity: 0, x: 20, height: 0 },
+      })
+    return <>{trail.map(({opacity, ...style}, index) => {
+        return <animated.span key={index} style={style} {...props}>
+            <animated.span style={{opacity}}>{textItems[index]}</animated.span>
         </animated.span>
     })}</>;
 }
