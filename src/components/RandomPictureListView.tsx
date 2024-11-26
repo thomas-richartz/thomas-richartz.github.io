f155f12982840569749e0fe360b999a484e38c21
pce <445226+pce@users.noreply.github.com>
2024-11-26 16:01:56 +0100

    fix: remove unused lightbox

--- src/components/RandomPictureListView.tsx
+++ src/components/RandomPictureListView.tsx
@@ -3,7 +3,7 @@ import { GalleryImage } from "../types";
 import { IntenseImage } from "./IntenseImage";
 import { LightBoxImage } from "./LightBoxImage";
 import styles from "./RandomPictureListView.module.css";
-import lightBoxStyles from "./LightBoxImage.module.css"
+import lightBoxStyles from "./LightBoxImage.module.css";
 
 interface IRandomPictureListView {
   images: GalleryImage[];
@@ -14,12 +14,6 @@ export const RandomPictureListView = ({
   images,
   selectedImage,
 }: IRandomPictureListView): JSX.Element => {
-  const [lightboxImage, setLightboxImage] = useState<GalleryImage | null>(null);
-
-  const handleImageClick = (image: GalleryImage) => {
-    setLightboxImage(image);
-  };
-
   const nextImage = () => {
     // Implement logic for showing the next image in the lightbox
   };
@@ -31,11 +25,7 @@ export const RandomPictureListView = ({
   return (
     <div className={styles.imageGrid}>
       {images.map((image, index) => (
-        <div
-          key={`${image.title}-${index}`}
-          className={styles.imageContainer}
-          onClick={() => handleImageClick(image)} // Open lightbox on image click
-        >
+        <div key={`${image.title}-${index}`} className={styles.imageContainer}>
           <IntenseImage
             nextImage={nextImage}
             prevImage={prevImage}
@@ -43,17 +33,9 @@ export const RandomPictureListView = ({
             title={image.title}
             className={styles.randomImg}
             src={`assets/images/${image.filename}`}
-          />          
+          />
         </div>
       ))}
-      {lightboxImage && (
-        <LightBoxImage
-          onClick={() => setLightboxImage(null)} // Close lightbox on click
-          alt={lightboxImage.title}
-          src={`assets/images/${lightboxImage.filename}`}
-          className={lightBoxStyles.lightBoxImage}
-        />
-      )}
     </div>
   );
 };
