788982ae1f37cc86b568e05e8fb5df24e6157a66
pce <445226+pce@users.noreply.github.com>
2025-05-23 00:18:24 +0200

    feat: absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -13,5 +14,10 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
