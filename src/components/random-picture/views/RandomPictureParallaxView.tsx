34a072a16138dd23b1e73e0655b7a4aec3315792
pce <445226+pce@users.noreply.github.com>
2025-06-05 20:11:06 +0200

    refactor: remove scroll ref

--- src/components/random-picture/views/RandomPictureParallaxView.tsx
+++ src/components/random-picture/views/RandomPictureParallaxView.tsx
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import React, { useEffect, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { PerspectiveCamera, useTexture, Text } from "@react-three/drei";
-// import { OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { GalleryImage } from "@/types";
 import CameraController from "@/components/CameraController";
 import "@/materials/BlurImageMaterial";
@@ -117,40 +117,13 @@ export const RandomPictureParallaxView = ({
   loadRandomImages,
   setImages,
 }: RandomPictureParallaxViewProps) => {
-  const scrollRef = useRef<HTMLDivElement>(null);
-  const [scrollY, setScrollY] = useState(0);
-  const [targetPosition, setTargetPosition] = useState<
-    [number, number, number]
-  >([0, 0, 10]);
+  const defaultCamTargetPos: [number, number, number] = [0, 0, 10];
+  const [targetPosition, setTargetPosition] =
+    useState<[number, number, number]>(defaultCamTargetPos);
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   const [windowOffset, setWindowOffset] = useState(0);
   const windowSize = 9;
   const cameraRef = useRef<any>();
-
-  const onScroll = () => {
-    if (scrollRef.current) {
-      setScrollY(scrollRef.current.scrollTop);
-    }
-  };
-
-  useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.addEventListener("scroll", onScroll);
-    }
-    return () => {
-      if (scrollRef.current) {
-        scrollRef.current.removeEventListener("scroll", onScroll);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    // scroll down = zoom out
-    const depth = 10 + scrollY / 100;
-    if (selectedIndex === null) {
-      setTargetPosition([0, 0, depth]);
-    }
-  }, [scrollY, selectedIndex]);
 
   const shiftWindow = () => {
     const nextOffset = windowOffset + 1;
@@ -161,69 +134,63 @@ export const RandomPictureParallaxView = ({
   };
 
   return (
-    <div ref={scrollRef} style={{ height: "100vh", overflowY: "scroll" }}>
-      <div style={{ height: `${images.length * 150}px` }}></div>
-      <Canvas
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          // pointerEvents: selectedIndex !== null ? "auto" : "none",
-          background: "black",
-        }}
-      >
-        <ambientLight intensity={0.5} />
-        {/* <directionalLight position={[5, 5, 5]} intensity={0.8} castShadow /> */}
-        <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
-        <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 0, 10]} />
+    <Canvas
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        // pointerEvents: selectedIndex !== null ? "auto" : "none",
+        background: "black",
+      }}
+    >
+      <ambientLight intensity={0.5} />
+      {/* <directionalLight position={[5, 5, 5]} intensity={0.8} castShadow /> */}
+      <directionalLight position={[5, 10, 5]} intensity={1} castShadow />
+      <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 0, 10]} />
 
-        <mesh position={[0, 0, -150]}>
-          <planeGeometry args={[window.innerWidth, window.innerHeight]} />
-          <meshBasicMaterial color="#1a1a1a" />
-        </mesh>
+      <mesh position={[0, 0, -150]}>
+        <planeGeometry args={[window.innerWidth, window.innerHeight]} />
+        <meshBasicMaterial color="#1a1a1a" />
+      </mesh>
 
-        {images.map((img, i) => {
-          const z = -i * 2;
-          const x = ((i % 3) - 1) * 3;
-          const y = Math.floor(i / 3) * -2.5;
-          return (
-            <ParallaxCube
-              selected={selectedIndex === i ? true : false}
-              key={img.filename}
-              image={img.filename}
-              title={img.title}
-              position={[x, y, z]}
-              onClick={() => {
-                if (selectedIndex === i) {
-                  // Zoom out to overview
-                  setSelectedIndex(null);
-                  setTargetPosition([0, 0, 10]); // TODO or a saved overview, prev pos
-                  // setTargetPosition([x, y, z - 4]); // zoom out
-                  // setTargetPosition([x, y, z - 4]); // zoom out
-                } else {
-                  // Zoom into selected cube
-                  setSelectedIndex(i);
-                  shiftWindow();
-                  setTargetPosition([x, y, z + 3.3]);
-                }
-              }}
-              blur={selectedIndex === i ? 0.0 : 2.5}
-            />
-          );
-        })}
-        <CameraController
-          cameraRef={cameraRef}
-          targetPosition={targetPosition}
-        />
-        {/* {selectedIndex !== null && (
-          <OrbitControls
-            enableZoom
-            enablePan={false}
-            enableRotate
-            target={targetPosition}
+      {images.map((img, i) => {
+        const z = -i * 2;
+        const x = ((i % 3) - 1) * 3;
+        const y = Math.floor(i / 3) * -1.5;
+        return (
+          <ParallaxCube
+            selected={selectedIndex === i ? true : false}
+            key={img.filename}
+            image={img.filename}
+            title={img.title}
+            position={[x, y, z]}
+            onClick={() => {
+              if (selectedIndex === i) {
+                // Zoom out to overview
+                setSelectedIndex(null);
+                setTargetPosition(defaultCamTargetPos); // TODO or a saved overview, prev pos
+                // setTargetPosition([x, y, z - 4]); // zoom out
+                // setTargetPosition([x, y, z - 4]); // zoom out
+              } else {
+                // Zoom into selected cube
+                setSelectedIndex(i);
+                shiftWindow();
+                setTargetPosition([x, y, z + 3.3]);
+              }
+            }}
+            blur={selectedIndex === i ? 0.0 : 2.5}
           />
-        )} */}
-      </Canvas>
-    </div>
+        );
+      })}
+      <CameraController cameraRef={cameraRef} targetPosition={targetPosition} />
+      {selectedIndex !== null && (
+        <OrbitControls
+          enableZoom
+          enablePan={false}
+          enableRotate
+          target={targetPosition}
+        />
+      )}
+    </Canvas>
   );
 };
