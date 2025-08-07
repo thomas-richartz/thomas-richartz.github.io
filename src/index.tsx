07bd845e13a51cee748fbe980d77adbccbf22e39
pce <info@pc-e.org>
2025-08-07 15:57:35 +0200

    fix: upgrade target to allow downlevelIteration

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Profiler } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-if (process.env.NODE_ENV !== 'development' && window.location.protocol === 'http:') {
-  window.location.href = window.location.href.replace(/^http:/, 'https:');
+if (process.env.NODE_ENV !== "development" && window.location.protocol === "http:") {
+  window.location.href = window.location.href.replace(/^http:/, "https:");
 }
 
 // console.log(process.env);
 
+// const onRender = (id, phase, actualDuration, baseDuration, startTime, commitTime, interactions) => {
+//   console.log(`%c${id} ${phase} ${actualDuration} ${baseDuration} ${startTime} ${commitTime} ${interactions}`, "color: blue");
+// };
+
 root.render(
   <React.StrictMode>
+    {/*  <Profiler
+        id="App"
+        onRender={onRender}
+    >
+        <App />
+      </Profiler> */}
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
